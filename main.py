5661b9a19e9e04a35fbfbaa3c791f6c7813e3476
lewis <lu5a@proton.me>
2025-01-26 14:33:14 +0200

    feat: better html, added delete endpoint

--- main.py
+++ main.py
@@ -80,38 +80,62 @@ def fetch_advertisements_html():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     cursor.execute(
-        "SELECT id, title, description FROM Advertisement WHERE active = 1 ORDER BY created_at DESC;"
+        "SELECT id, title, description, created_at, updated_at FROM Advertisement WHERE active = 1 ORDER BY created_at DESC;"
     )
     ads = cursor.fetchall()
     conn.close()
 
-    html = "<ul>"
-    for id, title, description in ads:
-        html += f"""<li>
+    html = "<div>"
+    for id, title, description, created_at, updated_at in ads:
+        html += f"""<div class="ad-container">
         <details>
-        <summary>{title} - {description}</summary>
-        <p><b>{title}</b>
-        <br />
-        {description}
-        <br />
-        Send a reply to the poster!
-        </p>
+        <summary>{title}</summary>
+        <i>Created on {created_at}{f", updated on {updated_at}" if updated_at is not None else ""}</i>
+        <p style="margin-left: 50px;">{description}</p>
         <form action="/send-message" method="post">
+            <span>Get in touch with the poster!</span>
             <input type="hidden" id="ad_id" name="ad_id" value="{id}" required>
 
-            <label for="user_email">Your Email:</label><br>
-            <input type="email" id="user_email" name="user_email" required>
-            <br />
+            <div class="input-container">
+                <label for="user_email">
+                    <b>Your email</b>
+                    <br />
+                    This won't be stored at all, it's just to tell the poster who to reply to.
+                    <br />
+                    Check the <a target="_blank" href="https://github.com/lu1a/kryptori">source code</a> if in doubt.
+                    <br />
+                    I'm open to suggestions on how to more formally prove that it's not
+                    <br />
+                    saved behind the scenes or anything.
+                </label>
+                <br />
+                <input type="email" id="user_email" name="user_email" required>
+            </div>
 
-            <label for="message">Message:</label><br>
-            <textarea id="message" name="message" rows="4" required></textarea>
-            <br />
+            <div class="input-container">
+                <label for="message">
+                    <b>Message</b>
+                </label>
+                <br />
+                <textarea id="message" name="message" style="width: 100%; height: 6rem;" required></textarea>
+            </div>
 
-            <button type="submit">Send Message</button>
+            <p>
+            Remember, this will just send an email to the poster.
+            <br />
+            Then you two will hash it out over email.
+            <br />
+            Don't come crying to me if the poster doesn't get back to you,
+            <br />
+            or if the poster grifts you or if you grift the poster.
+            </p>
+            <div class="input-container">
+                <button type="submit">Send Message</button>
+            </div>
         </form>
         </details>
-        </li>"""
-    html += "</ul>"
+        </div>"""
+    html += "</div>"
     return html
 
 
@@ -238,6 +262,20 @@ async def update_ad(
     )
 
 
+@app.post("/delete-ad", response_class=HTMLResponse)
+async def delete_at(
+    token: str = Form(...),
+):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM Advertisement WHERE owner_token = ?", (token,))
+    conn.commit()
+    conn.close()
+
+    return RedirectResponse(url="/?delete-success=true", status_code=303)
+
+
 @app.get("/manage-ad", response_class=HTMLResponse)
 async def manage_ad(request: Request):
     token = request.query_params.get("token")
@@ -247,9 +285,10 @@ async def manage_ad(request: Request):
     with open("pages/manage_ad.html", "r") as f:
         manage_ad_page = f.read()
 
-    title, description, created_at, updated_at, owner_email = (
-        fetch_advertisement_by_token(token)
-    )
+    ad = fetch_advertisement_by_token(token)
+    if not ad:
+        return RedirectResponse(url="/", status_code=303)
+    title, description, created_at, updated_at, owner_email = ad
     template = Template(manage_ad_page)
     rendered_page = template.render(
         title=title,
